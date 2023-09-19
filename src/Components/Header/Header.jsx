b1586e5d32ac45b90862c22d354dbe59665a9a01
ShahabMat <cse_1812020090@lus.ac.bd>
2023-09-19 19:10:48 +0600

    completed fake data making

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+ 
+
+const Header = () => {
+    return (
+        <div>
+             <h1 className='text-5xl text-center font-semibold'> Knowledge Xone </h1>
+        </div>
+    );
+};
+
+export default Header;
